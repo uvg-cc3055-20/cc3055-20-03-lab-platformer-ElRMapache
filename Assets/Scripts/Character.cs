d433d34f323a2e603161f99f9d36ae93fd765ab9
ElRMapache <alo15470@uvg.edu.gt>
2018-01-31 08:53:41 -0600

    lab 3
    Laboratorio Terminado

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,12 +11,16 @@ public class Character : MonoBehaviour {
     private float speed = 5f;
     private float jumpForce = 250f;
     private bool facingRight = true;
+    Animator anim;
+    AudioSource audio;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
@@ -32,7 +36,9 @@ public class Character : MonoBehaviour {
 
         if (Input.GetButtonDown("Jump")) {
             rb2d.AddForce(Vector2.up*jumpForce);
+            audio.Play();
         }
 
+        anim.SetFloat("Speed", Mathf.Abs(move));
 	}
 }
